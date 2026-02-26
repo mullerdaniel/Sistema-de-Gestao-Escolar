4c786fba7e5bca0996b53813fc5c0721d662c5f8
Daniel Müller <miladaniel370@gmail.com>
2026-02-26 16:10:07 -0300

    Feat: Finalização da classe cursoRepository

--- escola/src/main/java/com/example/escola/repository/CursoRepository.java
+++ escola/src/main/java/com/example/escola/repository/CursoRepository.java
@@ -5,6 +5,8 @@ import com.example.escola.utils.Conexao;
 import org.springframework.stereotype.Repository;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Repository
 public class CursoRepository {
@@ -14,19 +16,112 @@ public class CursoRepository {
     public Curso salvarCurso(Curso curso) throws SQLException {
         String query = "INSERT INTO curso (nome, codigo) VALUES (?,?)";
 
-        try(Connection conn = Conexao.conectar();
-            PreparedStatement stmt = conn.prepareStatement(query, Statement.RETURN_GENERATED_KEYS)) {
+        try (Connection conn = Conexao.conectar();
+             PreparedStatement stmt = conn.prepareStatement(query, Statement.RETURN_GENERATED_KEYS)) {
 
             stmt.setString(1, curso.getNome());
             stmt.setString(2, curso.getCodigo());
             stmt.executeUpdate();
 
             ResultSet rs = stmt.getGeneratedKeys();
-            if(rs.next()) {
+            if (rs.next()) {
                 curso.setId(rs.getInt(1));
-                return curso;
             }
         }
         return curso;
+    }
+
+
+    // BUSCAR TODOS
+    public List<Curso> buscarCursos() throws SQLException {
+        List<Curso> listaCursos = new ArrayList<>();
+
+        String query = """
+                SELECT id,
+                nome,
+                codigo
+                FROM curso
+                """;
+
+        try (Connection conn = Conexao.conectar();
+             PreparedStatement stmt = conn.prepareStatement(query);
+             ResultSet rs = stmt.executeQuery()) {
+
+            while (rs.next()) {
+                listaCursos.add(new Curso(
+                        rs.getInt("id"),
+                        rs.getString("nome"),
+                        rs.getString("codigo")
+                ));
+            }
+        }
+
+        return listaCursos;
+    }
+
+
+    // BUSCAR POR ID
+    public Curso buscarCursoPorId(int id) throws SQLException {
+        String query = """
+                SELECT id,
+                nome,
+                codigo
+                FROM curso
+                WHERE id = ?
+                """;
+
+        try (Connection conn = Conexao.conectar();
+             PreparedStatement stmt = conn.prepareStatement(query)) {
+
+            stmt.setInt(1, id);
+
+            try (ResultSet rs = stmt.executeQuery()) {
+                if (rs.next()) {
+                    return new Curso(
+                            rs.getInt("id"),
+                            rs.getString("nome"),
+                            rs.getString("codigo")
+                    );
+                }
+            }
+        }
+
+        return null;
+    }
+
+
+    // ATUALIZAR
+    public Curso atualizarCurso(Curso curso) throws SQLException {
+        String query = """
+                UPDATE curso
+                SET nome = ?,
+                codigo = ?
+                WHERE id = ?
+                """;
+
+        try (Connection conn = Conexao.conectar();
+             PreparedStatement stmt = conn.prepareStatement(query)) {
+
+            stmt.setString(1, curso.getNome());
+            stmt.setString(2, curso.getCodigo());
+            stmt.setInt(3, curso.getId());
+
+            stmt.executeUpdate();
+        }
+
+        return curso;
+    }
+
+
+    // DELETAR POR ID
+    public void deletarCursoPorId(int id) throws SQLException {
+        String query = "DELETE FROM curso WHERE id = ?";
+
+        try (Connection conn = Conexao.conectar();
+             PreparedStatement stmt = conn.prepareStatement(query)) {
+
+            stmt.setInt(1, id);
+            stmt.executeUpdate();
+        }
     }
 }
