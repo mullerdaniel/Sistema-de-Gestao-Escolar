bc00bd67e89056a1f62ba8a19a2da03b7419b7c0
Daniel Müller <miladaniel370@gmail.com>
2026-03-06 18:53:21 -0300

    Fix: Adicionando validação

--- escola/src/main/java/com/example/escola/dto/AlunoRequisicaoDto.java
+++ escola/src/main/java/com/example/escola/dto/AlunoRequisicaoDto.java
@@ -1,11 +1,23 @@
 package com.example.escola.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
+
 import java.time.LocalDate;
 
 public record AlunoRequisicaoDto (
+        @NotNull(message = "O Nome do aluno é obrigatorio!")
+        @Size(min = 3, max = 100, message = "O nome deve ter de 3 á 100 caracteres!")
         String nome,
+        @NotNull(message = "O Email do aluno é obrigatorio!")
+        @Email(message = "Email Invalido!")
         String email,
+        @NotNull(message = "A Matricula do aluno é obrigatorio!")
         String matricula,
+        @NotNull(message = "A Data de Nascimento do aluno é obrigatorio!")
+        @Past(message = "A Data de Nascimento deve ser no passado!")
         LocalDate data_nascimento
 ){
 }
