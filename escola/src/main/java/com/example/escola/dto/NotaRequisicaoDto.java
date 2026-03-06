30e6befe5188ea350a394901fe6b480f87897335
Daniel Müller <miladaniel370@gmail.com>
2026-03-06 19:15:24 -0300

    Fix: Adicionando validação

--- escola/src/main/java/com/example/escola/dto/NotaRequisicaoDto.java
+++ escola/src/main/java/com/example/escola/dto/NotaRequisicaoDto.java
@@ -1,5 +1,7 @@
 package com.example.escola.dto;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
@@ -10,7 +12,8 @@ public record NotaRequisicaoDto (
         @NotNull(message = "O Id da aula é obrigatorio!")
         int aula_id,
         @NotNull(message = "Deve ter uma nota obrigatoriamente!")
-        @Size(min = 0, max = 100, message = "A nota deve ser de 0 á 100")
+        @Min(0)
+        @Max(100)
         double valor
 ){
 }
