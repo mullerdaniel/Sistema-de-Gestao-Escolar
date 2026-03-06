5f9588cae1bfbf2ab3ee00b39ce59d9914242714
Daniel Müller <miladaniel370@gmail.com>
2026-03-06 18:55:53 -0300

    Fix: Adicionando validação

--- escola/src/main/java/com/example/escola/dto/AulaRequisicaoDto.java
+++ escola/src/main/java/com/example/escola/dto/AulaRequisicaoDto.java
@@ -1,10 +1,18 @@
 package com.example.escola.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 import java.time.LocalDate;
 
 public record AulaRequisicaoDto (
+        @NotNull(message = "O Id da turma é obrigatorio!")
         int turma_id,
+        @NotNull(message = "A data da aula é obrigatoria!")
         LocalDate data_hora,
+        @NotBlank(message = "O assunto da aula é obrigatorio!")
+        @Size(max = 300, message = "O assunto da aula deve ter no maximo 300 caracteres!")
         String assunto
 ){
 }
