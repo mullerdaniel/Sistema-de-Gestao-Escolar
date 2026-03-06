fee2c316fe368ab1fa5f2f6de4e44e607081d334
Daniel Müller <miladaniel370@gmail.com>
2026-03-06 19:03:37 -0300

    Fix: Adicionando validação

--- escola/src/main/java/com/example/escola/dto/TurmaRequisicaoDto.java
+++ escola/src/main/java/com/example/escola/dto/TurmaRequisicaoDto.java
@@ -1,8 +1,13 @@
 package com.example.escola.dto;
 
+import jakarta.validation.constraints.NotNull;
+
 public record TurmaRequisicaoDto (
+        @NotNull(message = "O nome da turma é obrigatorio!")
         String nome,
+        @NotNull(message = "O Id do curso é obrigatorio!")
         int curso_id,
+        @NotNull(message = "O Id do professor é obrigatorio")
         int professor_id
 ){
 }
